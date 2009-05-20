f5e7079ffeb6486c31cf5f2451475afbd6eec5ab
Baris Metin <baris@metin.org>
2009-05-20 16:52:01 +0200

    Initial commit

---
+++ ack/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from pisi.actionsapi import pisitools
+
+WorkDir="."
+
+def setup():
+    pass
+
+def build():
+    pass
+
+def install():
+    pisitools.dobin("ack")
